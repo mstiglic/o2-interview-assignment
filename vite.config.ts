d9c3df93dbc5c867d2b75bab6fe9167c738bf1a3
milan_stiglic <stiglic.work@gmail.com>
2025-02-17 20:27:56 +0100

    Add aliases for constants and helpers; Pre-pare the testing env

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
@@ -8,10 +8,17 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
-            '@components': path.resolve(__dirname,'./src/components'),
-            '@stories': path.resolve(__dirname,'./src/stories'),
-            '@assets': path.resolve(__dirname,'./src/assets'),
-            '@styles': path.resolve(__dirname,'./src/assets/styles'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@stories': path.resolve(__dirname, './src/stories'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@styles': path.resolve(__dirname, './src/assets/styles'),
+            '@constants': path.resolve(__dirname, './src/constants'),
+            '@helpers': path.resolve(__dirname, './src/helpers'),
         },
+    },
+    test: {
+        globals: true,        // Enables global test functions like `describe`, `test`
+        environment: 'jsdom', // Simulates a browser environment for testing
+        setupFiles: './src/setupTests.ts', // Runs before each test file
     },
 });
